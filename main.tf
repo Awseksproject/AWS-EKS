34b88d861758cbfd4790f089996b11a16ed71aa3
Awseksproject <162966386+Awseksproject@users.noreply.github.com>
2024-03-14 14:55:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ root_domain_name = "helloworld.info"
 }
 module "rds_instance" {
 source = "./Modules/AmazonRDS"
-db_password = "test@db1512"
+db_password = "dbpassword"
 aws_public_subnet = module.vpc.aws_public_subnet
 vpc_id = module.vpc.vpc_id
 }
