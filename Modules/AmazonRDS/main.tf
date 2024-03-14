53e72f33b925333d31cd46bbb9101c1cdc64b7bd
Awseksproject <162966386+Awseksproject@users.noreply.github.com>
2024-03-14 13:42:05 +0530

    Update main.tf

--- Modules/AmazonRDS/main.tf
+++ Modules/AmazonRDS/main.tf
@@ -61,7 +61,7 @@ resource "aws_db_instance" "tessolve_rds" {
   engine                 = "postgres"
   engine_version         = "14.1"
   username               = "tessolve"
-  password               = var.db_password
+  password               = "Testdb@12345"
   db_subnet_group_name   = aws_db_subnet_group.tessolve_rds.name
   vpc_security_group_ids = [aws_security_group.rds.id]
   parameter_group_name   = aws_db_parameter_group.tessolve_rds.name
