753447a19f9ab9155ddda7493c6c00b23aa4ca4e
Awseksproject <162966386+Awseksproject@users.noreply.github.com>
2024-03-15 12:03:31 +0530

    Update outputs.tf

--- Modules/AmazonRDS/outputs.tf
+++ Modules/AmazonRDS/outputs.tf
@@ -1,17 +1,17 @@
 output "rds_hostname" {
   description = "RDS instance hostname"
-  value       = aws_db_instance.tessolverds.address
+  value       = aws_db_instance.tessolverds2.address
   sensitive   = true
 }
 
 output "rds_port" {
   description = "RDS instance port"
-  value       = aws_db_instance.tessolverds.port
+  value       = aws_db_instance.tessolverds2.port
   sensitive   = true
 }
 
 output "rds_username" {
   description = "RDS instance root username"
-  value       = aws_db_instance.tessolverds.username
+  value       = aws_db_instance.tessolverds2.username
   sensitive   = true
 }
